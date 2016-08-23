3a8d6fd1398a5510d781a97ac85665201550e5cd
Emmanuel Nyachoke <achachiez@gmail.com>
2016-08-23 16:10:17 +0300

    Add pdfkit

--- test.js
+++ test.js
@@ -1,5 +1,11 @@
 var bardcode = require("bardcode");
 var convertToPath = require("./svg-shape-to-path").convertToPath;
+var PDFDocument = require('pdfkit');
+var fs = require('fs');
 var barcodeString = bardcode.drawBarcode('svg', 'Hello', {});
 var svgPath = convertToPath(barcodeString);
-console.log(svgPath.path);
+var doc = new PDFDocument();
+doc.pipe(fs.createWriteStream('output.pdf'));
+doc.path(svgPath.path)
+  .fill("#000000");
+doc.end();
